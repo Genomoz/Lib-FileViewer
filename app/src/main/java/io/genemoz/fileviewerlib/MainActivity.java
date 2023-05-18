79ae4679417359014d1a02cf3c63542e5754331e
ahmmedrejowan <ahmmedrejowan@gmail.com>
2023-05-18 12:18:21 +0600

    Changes

---
+++ app/src/main/java/io/genemoz/fileviewerlib/MainActivity.java
@@ -0,0 +1,21 @@
+package io.genemoz.fileviewerlib;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import io.genemoz.fileviewerlib.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+}
