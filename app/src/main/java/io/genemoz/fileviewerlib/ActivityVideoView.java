d25967eb8d7faac8bdc21cc6fd7805f39fde6b27
ahmmedrejowan <ahmmedrejowan@gmail.com>
2023-05-19 08:16:17 +0600

    Changes

--- app/src/main/java/io/genemoz/fileviewerlib/ActivityVideoView.java
+++ app/src/main/java/io/genemoz/fileviewerlib/ActivityVideoView.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import io.genemoz.fileviewerlib.databinding.ActivityVideoViewBinding;
+
 public class ActivityVideoView extends AppCompatActivity {
+
+    ActivityVideoViewBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_video_view);
+        binding = ActivityVideoViewBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
     }
 }
