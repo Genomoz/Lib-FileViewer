be4ad8b17b3406ab08bb3e5493984636a7c6eb17
ahmmedrejowan <ahmmedrejowan@gmail.com>
2023-05-22 16:50:52 +0600

    Changes

--- app/src/main/java/io/genemoz/fileviewerlib/videoView/VideoViewInActivity.java
+++ app/src/main/java/io/genemoz/fileviewerlib/videoView/VideoViewInActivity.java
@@ -3,17 +3,21 @@ package io.genemoz.fileviewerlib.videoView;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
 
 import io.genemoz.fileviewer.videoView.CustomVideoView;
 import io.genemoz.fileviewerlib.databinding.ActivityVideoViewInActivityBinding;
 
 public class VideoViewInActivity extends AppCompatActivity {
 
-    private static final String VIDEO_URL = "/storage/emulated/0/Download/MP4 Downloader/Adele-Hello.mp4";
+    private static final String VIDEO_URL = "/storage/emulated/0/Download/FB Downloader/A.mp4";
     ActivityVideoViewInActivityBinding binding;
     private int mSeekPosition;
     private int cachedHeight;
@@ -29,17 +33,12 @@ public class VideoViewInActivity extends AppCompatActivity {
 
         setVideoAreaSize();
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        binding.videoView.start();
-                    }
-                });
-            }
-        }, 1000);
+        new Handler().postDelayed(() -> runOnUiThread(() -> {
+            binding.videoView.start();
+            File file = new File(VIDEO_URL);
+            binding.mediaController.setTitle(file.getName() + "");
+
+        }), 1000);
 
 
         binding.videoView.setMediaController(binding.mediaController);
@@ -55,6 +54,7 @@ public class VideoViewInActivity extends AppCompatActivity {
                     binding.videoLayout.setLayoutParams(layoutParams);
                     //GONE the unconcerned views to leave room for video and controller
                     binding.bottomLayout.setVisibility(View.GONE);
+
                 } else {
                     ViewGroup.LayoutParams layoutParams = binding.videoLayout.getLayoutParams();
                     layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
@@ -62,6 +62,10 @@ public class VideoViewInActivity extends AppCompatActivity {
                     binding.videoLayout.setLayoutParams(layoutParams);
                     binding.bottomLayout.setVisibility(View.VISIBLE);
                 }
+
+                switchTitleBar(!isFullscreen);
+
+
             }
 
             @Override
@@ -85,6 +89,19 @@ public class VideoViewInActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void switchTitleBar(boolean b) {
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            if (b) {
+                supportActionBar.show();
+                Log.e("TAG", "switchTitleBar: " + "show");
+            } else {
+                supportActionBar.hide();
+                Log.e("TAG", "switchTitleBar: " + "hide");
+            }
+        }
     }
 
 
