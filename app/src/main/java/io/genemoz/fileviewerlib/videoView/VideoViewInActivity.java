5b7c14fc695b5b2c7858007a70df870fb68ff7ff
ahmmedrejowan <ahmmedrejowan@gmail.com>
2023-05-22 10:31:11 +0600

    Changes

--- app/src/main/java/io/genemoz/fileviewerlib/videoView/VideoViewInActivity.java
+++ app/src/main/java/io/genemoz/fileviewerlib/videoView/VideoViewInActivity.java
@@ -1,18 +1,109 @@
 package io.genemoz.fileviewerlib.videoView;
 
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-import io.genemoz.fileviewerlib.R;
+import io.genemoz.fileviewer.videoView.CustomVideoView;
+import io.genemoz.fileviewerlib.databinding.ActivityVideoViewInActivityBinding;
 
 public class VideoViewInActivity extends AppCompatActivity {
+
+    private static final String VIDEO_URL = "https://file-examples.com/wp-content/uploads/2017/04/file_example_MP4_480_1_5MG.mp4";
+    ActivityVideoViewInActivityBinding binding;
+    private int mSeekPosition;
+    private int cachedHeight;
+    private boolean isFullscreen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_video_view_in_activity);
+        binding = ActivityVideoViewInActivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+
+        setVideoAreaSize();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        binding.videoView.start();
+                    }
+                });
+            }
+        }, 1000);
+
+
+        binding.videoView.setMediaController(binding.mediaController);
+
+        binding.videoView.setVideoViewCallback(new CustomVideoView.VideoViewCallback() {
+            @Override
+            public void onScaleChange(boolean isFullscreen) {
+                VideoViewInActivity.this.isFullscreen = isFullscreen;
+                if (isFullscreen) {
+                    ViewGroup.LayoutParams layoutParams = binding.videoLayout.getLayoutParams();
+                    layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                    layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
+                    binding.videoLayout.setLayoutParams(layoutParams);
+                    //GONE the unconcerned views to leave room for video and controller
+                    binding.bottomLayout.setVisibility(View.GONE);
+                } else {
+                    ViewGroup.LayoutParams layoutParams = binding.videoLayout.getLayoutParams();
+                    layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                    layoutParams.height = VideoViewInActivity.this.cachedHeight;
+                    binding.videoLayout.setLayoutParams(layoutParams);
+                    binding.bottomLayout.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onPause(MediaPlayer mediaPlayer) {
+
+            }
+
+            @Override
+            public void onStart(MediaPlayer mediaPlayer) {
+
+            }
+
+            @Override
+            public void onBufferingStart(MediaPlayer mediaPlayer) {
+
+            }
+
+            @Override
+            public void onBufferingEnd(MediaPlayer mediaPlayer) {
+
+            }
+        });
 
     }
+
+
+    private void setVideoAreaSize() {
+        binding.videoLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                int width = binding.videoLayout.getWidth();
+                cachedHeight = (int) (width * 405f / 720f);
+
+                ViewGroup.LayoutParams videoLayoutParams = binding.videoLayout.getLayoutParams();
+                videoLayoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                videoLayoutParams.height = cachedHeight;
+                binding.videoLayout.setLayoutParams(videoLayoutParams);
+                binding.videoView.setVideoPath(VIDEO_URL);
+                binding.videoView.requestFocus();
+            }
+        });
+    }
+
+
 }
