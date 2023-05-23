6f6eaaf3ab0ebcf6711d4a73eab4fb544b062a09
ahmmedrejowan <ahmmedrejowan@gmail.com>
2023-05-24 00:39:25 +0600

    Changes

--- app/src/main/java/io/genemoz/fileviewerlib/pdfView/PdfViewInActivity.java
+++ app/src/main/java/io/genemoz/fileviewerlib/pdfView/PdfViewInActivity.java
@@ -4,18 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 import io.genemoz.fileviewerlib.R;
 import io.genemoz.fileviewerlib.databinding.ActivityPdfViewBinding;
+import io.genemoz.fileviewerlib.databinding.ActivityPdfViewInActivityBinding;
 
 public class PdfViewInActivity extends AppCompatActivity {
 
-    ActivityPdfViewBinding binding;
+    ActivityPdfViewInActivityBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityPdfViewBinding.inflate(getLayoutInflater());
+        binding = ActivityPdfViewInActivityBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+
+        Objects.requireNonNull(getSupportActionBar()).setTitle("PDF View In Activity");
+
 
 
     }
