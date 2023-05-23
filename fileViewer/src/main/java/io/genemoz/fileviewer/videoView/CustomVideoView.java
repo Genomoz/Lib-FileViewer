2b465d536dd6c4d9a80709a855462ee0db915fa0
ahmmedrejowan <ahmmedrejowan@gmail.com>
2023-05-23 13:14:19 +0600

    Changes

--- fileViewer/src/main/java/io/genemoz/fileviewer/videoView/CustomVideoView.java
+++ fileViewer/src/main/java/io/genemoz/fileviewer/videoView/CustomVideoView.java
@@ -385,7 +385,7 @@ public class CustomVideoView extends SurfaceView
 
     public void setVideoPath(String path) {
         setVideoURI(Uri.parse(path));
-        Log.e(TAG, "setVideoPath: " + Uri.parse(path) );
+        Log.e(TAG, "setVideoPath: " + Uri.parse(path));
     }
 
 
