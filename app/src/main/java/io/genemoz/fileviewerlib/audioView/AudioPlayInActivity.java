fa8a60b7a458e88790f69b7f6d1b41abfb12ed34
ahmmedrejowan <ahmmedrejowan@gmail.com>
2023-05-24 09:09:18 +0600

    Changes

--- app/src/main/java/io/genemoz/fileviewerlib/audioView/AudioPlayInActivity.java
+++ app/src/main/java/io/genemoz/fileviewerlib/audioView/AudioPlayInActivity.java
@@ -12,5 +12,9 @@ public class AudioPlayInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_play_in);
+
+
     }
+
+
 }
