71ec0737d2d9cfe9be0f7212ceff08cb12b2432b
ahmmedrejowan <ahmmedrejowan@gmail.com>
2023-05-24 09:15:15 +0600

    Changes

--- app/src/main/java/io/genemoz/fileviewerlib/audioView/AudioPlayInActivity.java
+++ app/src/main/java/io/genemoz/fileviewerlib/audioView/AudioPlayInActivity.java
@@ -4,14 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.util.Objects;
+
 import io.genemoz.fileviewerlib.R;
+import io.genemoz.fileviewerlib.databinding.ActivityAudioPlayInBinding;
 
 public class AudioPlayInActivity extends AppCompatActivity {
+
+
+    ActivityAudioPlayInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_audio_play_in);
+        binding = ActivityAudioPlayInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Audio Title");
+
+        try {
+            binding.audioPlayer.setDataSource("/storage/emulated/0/Test/AUD.mp3/");
+            binding.audioPlayer.play();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
 
 
     }
